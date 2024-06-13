469a19ee409727ee291a7572b57018df735043b7
@dnetochaves <dnetochaves@gmail.com>
2024-06-13 08:46:12 -0300

    deletar

--- clientes/models.py
+++ clientes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Cliente(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     data_nascimento = models.DateField(null=False, blank=False)
--- clientes/views.py
+++ clientes/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 import datetime
-from django.views.generic import CreateView, ListView, UpdateView, DetailView
+from django.views.generic import (
+    CreateView,
+    ListView,
+    UpdateView,
+    DetailView,
+    DeleteView,
+)
 from .models import Cliente
 from django.urls import reverse_lazy
+
 
 # Create your views here.
 def clientes(request):
@@ -26,14 +33,21 @@ class ClienteListView(ListView):
     model = Cliente
     template_name = "lista_clientes.html"
 
+
 class ClienteUpdateView(UpdateView):
     model = Cliente
     fields = "__all__"
     template_name = "forms_clientes.html"
-    success_url = reverse_lazy("clientes_list") #"/clientes/clientes_list"
+    success_url = reverse_lazy("clientes_list")  # "/clientes/clientes_list"
+
 
 class ClienteDetailView(DetailView):
     model = Cliente
     fields = "__all__"
     template_name = "lista_detail_clientes.html"
     context_object_name = "cliente"
+
+
+class ClienteDeleteView(DeleteView):
+    model = Cliente
+    success_url = reverse_lazy("clientes_list")  # "/clientes/clientes_list"
